e257eb8408ff614d653f689aab5ec87b7c2e1a4e
Andrew Oxenburgh <andrew.oxenburgh@adscale.co.nz>
2012-10-16 08:05:50 +1300

    First commit

---
+++ src/main/groovy/org/adscale/KarateChop1/Chop.groovy
@@ -0,0 +1,41 @@
+package org.adscale.KarateChop1
+
+/**
+ * Copyright AdScale GmbH, Germany, 2007
+ */
+class Chop {
+
+    int chop(int needle, List<Integer> haystack) {
+        if (haystack.isEmpty()) {
+            return -1
+        }
+        if (haystack[0] == needle) {
+            return 0
+        }
+        if (haystack[haystack.size() - 1] == needle) {
+            return haystack.size() - 1
+        }
+        if (haystack.size() <= 2) {
+            return -1
+        }
+        int middle = haystack.size() / 2
+        if (haystack[middle] == needle) {
+            return middle
+        }
+        println "middle = $middle"
+        if (notAtEitherEnd(middle, haystack)) {
+            return haystack[middle]
+        }
+        if (needle < haystack[middle]) {
+            return chop(needle, haystack.subList(0, middle))
+        }
+        else {
+            return middle + chop(needle, haystack.subList(middle, haystack.size()))
+        }
+    }
+
+
+    private boolean notAtEitherEnd(int middle, List<Integer> haystack) {
+        middle == 0 || middle == haystack.size() - 1
+    }
+}
---
+++ src/test/groovy/KarateChop_Test.groovy
@@ -0,0 +1,67 @@
+import org.junit.Test
+
+import static org.junit.Assert.assertEquals
+
+import org.adscale.KarateChop1.Chop
+
+class KarateChop_Test {
+
+    def chopper = new Chop();
+
+    @Test
+    void emptyHaystack_returnsMinus1() {
+        checkKarateChop(-1, 1, [])
+    }
+
+    @Test
+    void haystack_withNeedleToLeft_returnsMinus1() {
+        checkKarateChop(-1, 1, [2])
+    }
+
+    @Test
+    void haystack_withNeedleToRight_returnsMinus1() {
+        checkKarateChop(-1, 3, [2])
+    }
+
+    @Test
+    void haystack_withoutNeedle_returnsMinus1() {
+        checkKarateChop(-1, 1, [2])
+    }
+
+    @Test
+    void haystackOfOne_withNeedle_returns0() {
+        checkKarateChop(0, 1, [1])
+    }
+
+    @Test
+    void haystackOfTwo_withNeedleFirst_returns0() {
+        checkKarateChop(0, 1, [1, 2])
+    }
+
+    @Test
+    void haystackOfTwo_withNeedleLast_returnsLengthMinus1() {
+        checkKarateChop(0, 1, [1, 2])
+        checkKarateChop(2, 3, [1, 2, 3])
+    }
+
+    @Test
+    void haystackOfOddNumber_withNeedleMiddle_returnsOffset() {
+        checkKarateChop(1, 2, [1, 2, 3])
+    }
+
+    @Test
+    void largish_haystack_works() {
+        def haystack = [1, 2, 3, 4, 5]
+        checkKarateChop(-1, 0, haystack)
+        checkKarateChop(0, 1, haystack)
+        checkKarateChop(1, 2, haystack)
+        checkKarateChop(2, 3, haystack)
+        checkKarateChop(3, 4, haystack)
+        checkKarateChop(4, 5, haystack)
+    }
+
+    private void checkKarateChop(int expected, int needle, ArrayList<Integer> haystack) {
+        assertEquals(expected, chopper.chop(needle, haystack))
+    }
+
+}
